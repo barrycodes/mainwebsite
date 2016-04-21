c2654f0c695f9ad71cb2c2c0ba9dc7ee74ab3527
Barry Smith <bsmith.has.email@gmail.com>
2016-04-21 14:48:29 -0500

    updated manifest

--- MainWebsite/BarryCodes/Properties/AssemblyInfo.cs
+++ MainWebsite/BarryCodes/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BarryCodes")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Home page of Barry Smith")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Barry Smith")]
 [assembly: AssemblyProduct("BarryCodes")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
