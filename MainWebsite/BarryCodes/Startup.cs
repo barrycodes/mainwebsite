3dcf8255dd01a95b8225fd7e7ba2703bfa3564ea
Barry Smith <bsmith.has.email@gmail.com>
2016-04-21 14:40:25 -0500

    created asp.net project from template with tests

---
+++ MainWebsite/BarryCodes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BarryCodes.Startup))]
+namespace BarryCodes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
